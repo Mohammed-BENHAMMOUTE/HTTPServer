b3caaaf3dacc6e6556dd984751265a0332962ba2
Mohammed-BENHAMMOUTE <mohammedbenhammoute@gmail.com>
2024-10-09 13:12:02 +0000

    [first try]

--- app/main.ts
+++ app/main.ts
@@ -1,13 +1,11 @@
 import * as net from "net";
-
-// You can use print statements as follows for debugging, they'll be visible when running tests.
+import { Buffer } from "buffer";
 console.log("Logs from your program will appear here!");
 
-// Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.on("close", () => {
+    socket.write(`HTTP/1.1 200 OK\r\n\r\n`);
     socket.end();
-  });
 });
+
 
 server.listen(4221, "localhost");
